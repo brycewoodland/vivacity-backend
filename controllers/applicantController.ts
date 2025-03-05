27dd2653cbfae3a3140b69c2dc637be4ebab3380
brycewoodland <bwoodlan@byui.edu>
2025-03-04 18:29:18 -0700

    Implemented Postgres database.

--- controllers/applicantController.ts
+++ controllers/applicantController.ts
@@ -1,6 +1,14 @@
 import { Request, Response } from 'express';
+import pool from '../database/db';
 
-export const getApplicantInformation = async(req: Request, res: Response) => {
-    res.send('Applicant Information');
+const getApplicantInformation = async(req: Request, res: Response) => {
+    try {
+        const result = await pool.query('SELECT * FROM personal_info');
+        res.json(result.rows);
+    } catch (err) {
+        console.error('Error querying the data.');
+        res.status(500).send('Server error.');
+    }
 };
 
+export default getApplicantInformation;
